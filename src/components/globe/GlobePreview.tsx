bf0653c9e6bdd2727bd34aa5218a2bab88d857b8
TheEndProductions <ethan.adams.fox@gmail.com>
2026-03-07 09:17:05 -0700

    Fix globe preview to orbit camera around globe

--- src/components/globe/GlobePreview.tsx
+++ src/components/globe/GlobePreview.tsx
@@ -1,23 +1,10 @@
 'use client';
 import { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Stars } from '@react-three/drei';
+import { Stars, OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
 import Earth from './shared/Earth';
 import ShootingStars from './shared/ShootingStars';
-import Atmosphere from './shared/Atmosphere';
-
-function SpinningGlobe() {
-  const groupRef = useRef<THREE.Group>(null);
-  useFrame((_, delta) => {
-    if (groupRef.current) groupRef.current.rotation.y += delta * 0.15;
-  });
-  return (
-    <group ref={groupRef}>
-      <Earth lowRes />
-    </group>
-  );
-}
 
 export default function GlobePreview() {
   return (
@@ -25,7 +12,14 @@ export default function GlobePreview() {
       <Stars radius={100} depth={50} count={1500} factor={5} saturation={0} fade speed={0.3} />
       <ambientLight intensity={0.15} />
       <directionalLight position={[5, 3, 5]} intensity={0.8} />
-      <SpinningGlobe />
+      <Earth lowRes />
+      <OrbitControls
+        enablePan={false}
+        enableZoom={false}
+        enableRotate={false}
+        autoRotate
+        autoRotateSpeed={0.4}
+      />
       <ShootingStars count={3} />
     </Canvas>
   );
